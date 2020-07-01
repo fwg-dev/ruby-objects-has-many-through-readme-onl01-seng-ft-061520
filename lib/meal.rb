d6d6782b9871d8f0d0020281ac5648885d5113fc
Faith  Gicheha <fwgicheh@gmail.com>
2020-07-01 17:28:48 +0000

    Done.

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,8 +1,16 @@
 class Meal
+  @@all = []
 def initialize(waiter, customer, total, tip)
   @waiter = waiter
   @customer = Customer
   @total = total
   @tip = tip
+  @@all << seld
 end
+
+def self.all
+  @@all
+end
+
+
 end
