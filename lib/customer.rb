440424f9af449abd64a799f7c4272b0d793c1f6d
Faith  Gicheha <fwgicheh@gmail.com>
2020-07-02 02:27:19 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -13,9 +13,9 @@ class Customer
     @@all
   end
 
-# def new_meal(waiter, total, tip)
-#   Meal.new(waiter, self, total, tip)
-# end
+def new_meal(waiter, total, tip)
+  Meal.new(waiter, self, total, tip)
+end
 # def meals
 #   Meal.all.select do |meal|
 #     meal.customer == self
